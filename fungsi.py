b9d2c38ed802074de6aa25e5a3afe6b0afbe8e7a
irmaamanda <130949597+irmaamanda@users.noreply.github.com>
2023-05-27 22:13:35 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -9,28 +9,21 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 def make_model():
-  
-NUM_CLASSES = 3
-INPUT_SHAPE = IMAGE_SIZE +(3,)
-model = tf.keras.Sequential([
-    
+  model = tf.keras.Sequential([
     # rescaling and Data augmentation block
     tf.keras.layers.RandomFlip("horizontal_and_vertical", input_shape=INPUT_SHAPE),
     tf.keras.layers.RandomRotation(0.2),
     tf.keras.layers.RandomZoom(0.2),
     tf.keras.layers.Rescaling(1.0/255),
-    
     # Conv2D Block
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(),
-    
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(),
-    
     # Flatten Block
     tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Flatten(),
@@ -38,7 +31,6 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
     
 ],name = "Mido")
-
 
 # plot the model
 keras.utils.plot_model(model, show_shapes=True)
