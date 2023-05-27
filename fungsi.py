10bb7b71f77725bdb5982eda3c3726161d7e9e6c
irmaamanda <130949597+irmaamanda@users.noreply.github.com>
2023-05-27 22:21:06 +0700

    Update fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -9,13 +9,13 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 def make_model():
+  NUM_CLASSES = 3
+  INPUT_SHAPE = IMAGE_SIZE +(3,)
   model = tf.keras.Sequential([
-    # rescaling and Data augmentation block
     tf.keras.layers.RandomFlip("horizontal_and_vertical", input_shape=INPUT_SHAPE),
     tf.keras.layers.RandomRotation(0.2),
     tf.keras.layers.RandomZoom(0.2),
     tf.keras.layers.Rescaling(1.0/255),
-    # Conv2D Block
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
@@ -24,7 +24,6 @@ def make_model():
     tf.keras.layers.MaxPooling2D(),
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(),
-    # Flatten Block
     tf.keras.layers.Dropout(0.4),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
